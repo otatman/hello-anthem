6e0c160795c077416df970633ec150d33f4a832c
otatman <53582658+otatman@users.noreply.github.com>
2020-01-26 16:46:00 -0500

    Update HelloAnthemApplication.java

--- src/main/java/com/example/demo/HelloAnthemApplication.java
+++ src/main/java/com/example/demo/HelloAnthemApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RestController
 @SpringBootApplication
 public class HelloAnthemApplication {
 
